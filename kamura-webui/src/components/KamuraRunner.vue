1e11cc5fa9eb3f0b980473442c1fcd3d82b9be85
B1ACK917 <397615103@qq.com>
2024-08-31 18:45:47 +0800

    Update: ui runner page

--- kamura-webui/src/components/KamuraRunner.vue
+++ kamura-webui/src/components/KamuraRunner.vue
@@ -1,16 +1,20 @@
 <template>
   <el-container class="kamura-runner" style="height: 800px">
-    <el-aside width="200px">
+    <el-aside width="400px">
       <el-scrollbar>
         <el-menu>
           <el-sub-menu index="1">
             <template #title>
-              Traces
+              Workloads
             </template>
             <el-menu-item-group>
-              <el-menu-item index="1-1">hello</el-menu-item>
-              <el-menu-item index="1-2">dhrystone</el-menu-item>
-              <el-menu-item index="1-3">quicksort</el-menu-item>
+              <el-menu-item index="1-1" @click="addTaskToRunner('hello_trace','trace')">hello_trace</el-menu-item>
+              <el-menu-item index="1-2" @click="addTaskToRunner('dhry_riscv','trace')">dhry_riscv</el-menu-item>
+              <el-menu-item index="1-3" @click="addTaskToRunner('core_riscv','trace')">core_riscv</el-menu-item>
+              <el-menu-item index="1-4" @click="addTaskToRunner('dependency-p','elf')">dependency-p</el-menu-item>
+              <el-menu-item index="1-5" @click="addTaskToRunner('store-p','elf')">store-p</el-menu-item>
+              <el-menu-item index="1-6" @click="addTaskToRunner('store_simple-p','elf')">store_simple-p</el-menu-item>
+              <el-menu-item index="1-7" @click="addTaskToRunner('test-p','elf')">test-p</el-menu-item>
             </el-menu-item-group>
           </el-sub-menu>
           <el-sub-menu index="2">
@@ -18,9 +22,12 @@
               Runners
             </template>
             <el-menu-item-group>
-              <el-menu-item index="2-1">20240831-simple-hello</el-menu-item>
-              <el-menu-item index="2-2">20240831-simple-dhrystone</el-menu-item>
-              <el-menu-item index="2-3">20240831-simple-quicksort</el-menu-item>
+              <el-menu-item v-for="task in tasks" :key="task.uuid" @click="fetchTaskLog(task.uuid)">
+                <el-icon :style="{ color: getStatusColor(task.status) }">
+                  <component :is="getIconComponent(task.status)"/>
+                </el-icon>
+                {{ task.uuid }}
+              </el-menu-item>
             </el-menu-item-group>
           </el-sub-menu>
         </el-menu>
@@ -28,22 +35,123 @@
     </el-aside>
 
     <el-main>
-      <div ref="cyRef" style="width: 99%; height: 600px; text-align: left" class="cyRef"></div>
+      <div>
+        <textarea id="log" readonly></textarea>
+      </div>
     </el-main>
-
   </el-container>
 </template>
 
-
 <script>
+import axios from 'axios';
+import {SuccessFilled, CircleCloseFilled, QuestionFilled} from '@element-plus/icons-vue';
+
 export default {
   name: 'KamuraRunner',
   data() {
-    return {};
+    return {
+      tasks: [],  // Store tasks with status and uuid
+      taskLog: '', // Store the log of the selected task
+    };
+  },
+  created() {
+    // Fetch tasks every second
+    setInterval(this.fetchAllTasks, 2500);
+  },
+  methods: {
+    async fetchAllTasks() {
+      try {
+        const response = await axios.get('http://localhost:9999/getAllTasks');
+        if (response.data.success) {
+          this.tasks = response.data.tasks.map(task => ({
+            uuid: task,
+            status: 'Loading'
+          }));
+          await this.updateTaskStatuses();
+        }
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    },
+    async updateTaskStatuses() {
+      for (let task of this.tasks) {
+        try {
+          const statusResponse = await axios.post('http://localhost:9999/getTaskStatus', {uuid: task.uuid});
+          if (statusResponse.data.success) {
+            task.status = statusResponse.data.message;
+          } else {
+            task.status = 'Unknown';
+          }
+        } catch (error) {
+          console.error('Error updating task status:', error);
+        }
+      }
+    },
+    async fetchTaskLog(uuid) {
+      try {
+        const response = await axios.post('http://localhost:9999/getTaskLog', {uuid});
+        if (response.data.success) {
+          // this.taskLog = response.data.log;
+          let responseSpan = document.getElementById("log")
+          responseSpan.innerHTML = response.data.message
+        }
+      } catch (error) {
+        console.error('Error fetching task log:', error);
+      }
+    },
+    getIconComponent(status) {
+      switch (status) {
+        case 'Succeed':
+          return SuccessFilled;
+        case 'Failed':
+          return CircleCloseFilled;
+        case 'Running':
+          return QuestionFilled;
+        default:
+          return QuestionFilled; // Default icon if status is unknown
+      }
+    },
+    getStatusColor(status) {
+      switch (status) {
+        case 'Succeed':
+          return 'green';
+        case 'Failed':
+          return 'red';
+        case 'Running':
+          return 'yellow';
+        default:
+          return 'gray'; // Default color if status is unknown
+      }
+    },
+    async addTaskToRunner(workload, workloadType) {
+      try {
+        const response = await axios.post('http://localhost:9999/addTask', {
+          arch: 'simple',
+          workload: workload,
+          workload_type: workloadType
+        }, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        console.log('Response:', response.data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#log {
+  width: 98%;
+  height: 700px;
+  margin: 0 auto;
+  padding: 10px;
+  text-align: left;
+  border-radius: 4px;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
 </style>
